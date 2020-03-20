296e7c71c9414e123bd2a13d870a16702f08bceb
Alex Chiu <alexchiu11@gmail.com>
2020-03-20 16:46:53 +0000

    setup react router and routes for all fcc challenges

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Home from './pages/Home'
+import RandomQuoteMachine from './pages/RandomQuoteMachine'
+import MarkdownPreviewer from './pages/MarkdownPreviewer'
+import DrumMachine from './pages/DrumMachine'
+import Calculator from './pages/Calculator'
+import PomodoroClock from './pages/PomodoroClock'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/random-quote-machine">Random Quote Machine</Link>
+        </li>
+        <li>
+          <Link to="/markdown-previewer">Markdown Previewer</Link>
+        </li>
+        <li>
+          <Link to="/drum-machine">Drum Machine</Link>
+        </li>
+        <li>
+          <Link to="/js-calculator">JS Calculator</Link>
+        </li>
+        <li>
+          <Link to="/pomodoro-clock">Pomodoro Clock</Link>
+        </li>
+      </ul>
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/random-quote-machine">
+          <RandomQuoteMachine />
+        </Route>
+        <Route path="/markdown-previewer">
+          <MarkdownPreviewer />
+        </Route>
+        <Route path="/drum-machine">
+          <DrumMachine />
+        </Route>
+        <Route path="/js-calculator">
+          <Calculator />
+        </Route>
+        <Route path="/pomodoro-clock">
+          <PomodoroClock />
+        </Route>
+      </Switch>
+    </Router >
+
   );
 }
 
