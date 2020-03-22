306dc99ea1b8c34ccc24d6a1e37a24d22eb35d6d
Alex Chiu <alexchiu11@gmail.com>
2020-03-22 16:28:34 +0000

    setup ilayout and some basic styling

--- src/pages/MarkdownPreviewer/index.js
+++ src/pages/MarkdownPreviewer/index.js
@@ -4,7 +4,28 @@ import './MarkdownPreviewer.scss'
 export default function RandomQuoteMachine() {
   return (
     <main id="markdown-previewer">
-      <div>WIP!🏗️</div>
+      <Editor />
+      <Preview />
     </main>
   );
+}
+
+function Editor() {
+  return (
+    <div id="editor-container">
+      <div>Editor</div>
+      <textarea name="" id="editor" ></textarea>
+    </div>
+  )
+}
+
+function Preview() {
+  return (
+    <div id="preview-container">
+      <div>Preview</div>
+      <div id="preview">
+      </div>
+    </div>
+  )
+
 }
