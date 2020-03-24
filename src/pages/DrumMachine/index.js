624d6de272f4ee5e4d669e692be3b3b56a968bcb
Alex Chiu <alexchiu11@gmail.com>
2020-03-24 16:45:03 +0000

    keypress linked up to unique audio bites

--- src/pages/DrumMachine/index.js
+++ src/pages/DrumMachine/index.js
@@ -11,39 +11,56 @@ class DrumMachine extends React.Component {
     113: {
       id: "Q",
       handleKeyPress: this.handleKeyPress,
-      clip: "https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3",
+      name: "Chord-1",
+      url: "https://s3.amazonaws.com/freecodecamp/drums/Chord_1.mp3",
     },
     119: {
       id: "W",
       handleKeyPress: this.handleKeyPress,
+      name: "Chord-2",
+      url: "https://s3.amazonaws.com/freecodecamp/drums/Chord_2.mp3",
     },
     101: {
       id: "E",
       handleKeyPress: this.handleKeyPress,
+      name: "Chord-3",
+      url: "https://s3.amazonaws.com/freecodecamp/drums/Chord_3.mp3",
     },
     97: {
       id: "A",
       handleKeyPress: this.handleKeyPress,
+      name: "Shaker",
+      url: "https://s3.amazonaws.com/freecodecamp/drums/Give_us_a_light.mp3",
     },
     115: {
       id: "S",
       handleKeyPress: this.handleKeyPress,
+      name: "Open-HH",
+      url: "https://s3.amazonaws.com/freecodecamp/drums/Dry_Ohh.mp3",
     },
     100: {
       id: "D",
       handleKeyPress: this.handleKeyPress,
+      name: "Closed-HH",
+      url: "https://s3.amazonaws.com/freecodecamp/drums/Bld_H1.mp3",
     },
     122: {
       id: "Z",
       handleKeyPress: this.handleKeyPress,
+      name: "Punchy-Kick",
+      url: "https://s3.amazonaws.com/freecodecamp/drums/punchy_kick_1.mp3",
     },
     120: {
       id: "X",
       handleKeyPress: this.handleKeyPress,
+      name: "Side-Stick",
+      url: "https://s3.amazonaws.com/freecodecamp/drums/side_stick_1.mp3",
     },
     99: {
       id: "C",
       handleKeyPress: this.handleKeyPress,
+      name: "Snare",
+      url: "https://s3.amazonaws.com/freecodecamp/drums/Brk_Snr.mp3",
     },
   };
 
@@ -59,7 +76,8 @@ class DrumMachine extends React.Component {
   //key press handler used in keyPressLookup object
   handleKeyPress(keyPressed) {
     console.log(`Pressed ${keyPressed}`);
-    const sound = document.getElementById(keyPressed);
+    //the audio tag should be always be direct child of the
+    const sound = document.getElementById(keyPressed).childNodes[0];
     sound.play();
   }
 
@@ -71,52 +89,49 @@ class DrumMachine extends React.Component {
             <DrumPad
               keyPress={this.lookupKeyPress}
               id={this.keyPressLookup[113].id}
-              soundClip={this.keyPressLookup[113].clip}
+              soundClip={this.keyPressLookup[113].url}
             />
             <DrumPad
               keyPress={this.lookupKeyPress}
               id={this.keyPressLookup[119].id}
-              soundClip={this.keyPressLookup[119].clip}
+              soundClip={this.keyPressLookup[119].url}
             />
             <DrumPad
               keyPress={this.lookupKeyPress}
               id={this.keyPressLookup[101].id}
-              soundClip={this.keyPressLookup[101].clip}
+              soundClip={this.keyPressLookup[101].url}
             />
             <DrumPad
               keyPress={this.lookupKeyPress}
               id={this.keyPressLookup[97].id}
-              soundClip={this.keyPressLookup[97].clip}
+              soundClip={this.keyPressLookup[97].url}
             />
             <DrumPad
               keyPress={this.lookupKeyPress}
               id={this.keyPressLookup[115].id}
-              soundClip={this.keyPressLookup[115].clip}
+              soundClip={this.keyPressLookup[115].url}
             />
             <DrumPad
               keyPress={this.lookupKeyPress}
               id={this.keyPressLookup[100].id}
-              soundClip={this.keyPressLookup[100].clip}
+              soundClip={this.keyPressLookup[100].url}
             />
             <DrumPad
               keyPress={this.lookupKeyPress}
               id={this.keyPressLookup[122].id}
-              soundClip={this.keyPressLookup[122].clip}
+              soundClip={this.keyPressLookup[122].url}
             />
-            <button
-              id="X"
-              className="drum-pad"
-              onKeyPress={this.lookupKeyPress}
-            >
-              X
-            </button>
-            <button
-              id="C"
-              className="drum-pad"
-              onKeyPress={this.lookupKeyPress}
-            >
-              C
-            </button>
+            <DrumPad
+              keyPress={this.lookupKeyPress}
+              id={this.keyPressLookup[120].id}
+              soundClip={this.keyPressLookup[120].url}
+            />
+
+            <DrumPad
+              keyPress={this.lookupKeyPress}
+              id={this.keyPressLookup[99].id}
+              soundClip={this.keyPressLookup[99].url}
+            />
           </div>
         </div>
       </main>
@@ -127,6 +142,7 @@ class DrumMachine extends React.Component {
 function DrumPad(props) {
   return (
     <button
+      id={props.id}
       className="drum-pad"
       onKeyPress={props.keyPress}
       onClick={props.keyPress}
