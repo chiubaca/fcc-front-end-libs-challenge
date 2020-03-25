032835ee737c9e1ea78b08dbddd68264876695b5
Alex Chiu <alexchiu11@gmail.com>
2020-03-25 16:50:27 +0000

    implemented number and clear handler and conditional display rendering when calc state is empty the display shows 0

--- src/pages/Calculator/index.js
+++ src/pages/Calculator/index.js
@@ -4,32 +4,61 @@ import "./Calculator.scss";
 class Calculator extends React.Component {
   constructor(props) {
     super(props);
-
+    this.numberHandler = this.numberHandler.bind(this);
+    this.clearHandler = this.clearHandler.bind(this);
     this.state = {
-      inputA: null,
-      inputB: null,
-      function: null,
-      displayValue: "test",
+      inputA: "",
+      inputB: "",
+      function: "",
     };
+  }
+
+  numberHandler(e) {
+    const value = e.target.textContent;
+    this.setState({
+      inputA: this.state.inputA + value,
+      state: ""
+    });
+  }
+
+  clearHandler() {
+    this.setState({
+      inputA: "",
+      inputB: "",
+      function: "",
+    })
   }
 
   render() {
     return (
       <main id="calculator">
         <div id="calculator-container">
-          <div id="display">{this.state.displayValue}</div>
+          <div id="display">
+            {/* the display will diplay 0 if  there is no inital inputA*/}
+            {this.state.inputA === ""
+              ? "0"
+              : `${this.state.inputA} ${this.state.function} ${this.state.inputB}`}
+          </div>
           <div id="buttons-container">
             <div id="number-container">
-              <Number id="nine" value={9} /> <Number id="eight" value={8} />
-              <Number id="seven" value={7} />
-              <Number id="six" value={6} /> <Number id="five" value={5} />
-              <Number id="four" value={4} />
-              <Number id="three" value={3} /> <Number id="two" value={2} />
-              <Number id="one" value={1} />
-              <Number id="decimal" value="." /> <Number id="zero" value={0} />
+              <Number id="nine" value={9} clickHandler={this.numberHandler} />{" "}
+              <Number id="eight" value={8} clickHandler={this.numberHandler} />
+              <Number id="seven" value={7} clickHandler={this.numberHandler} />
+              <Number id="six" value={6} clickHandler={this.numberHandler} />
+              <Number id="five" value={5} clickHandler={this.numberHandler} />
+              <Number id="four" value={4} clickHandler={this.numberHandler} />
+              <Number id="three" value={3} clickHandler={this.numberHandler} />
+              <Number id="two" value={2} clickHandler={this.numberHandler} />
+              <Number id="one" value={1} clickHandler={this.numberHandler} />
+              <Number
+                id="decimal"
+                value="."
+                clickHandler={this.numberHandler}
+              />
+              <Number id="zero" value={0} clickHandler={this.numberHandler} />
             </div>
             <div id="function-container">
-              <Function id="clear" value="C" />
+              <button id="clear" onClick={this.clearHandler}>C</button>
               <Function id="add" value="+" />
               <Function id="subtract" value="-" />
               <Function id="multiply" value="*" />
@@ -44,11 +73,19 @@ class Calculator extends React.Component {
 }
 
 function Number(props) {
-  return <button id={props.id}>{props.value}</button>;
+  return (
+    <button onClick={props.clickHandler} id={props.id}>
+      {props.value}
+    </button>
+  );
 }
 
 function Function(props) {
-  return <button id={props.id}>{props.value}</button>;
+  return (
+    <button id={props.id} value={props.value}>
+      {props.value}
+    </button>
+  );
 }
 
 export default Calculator;
