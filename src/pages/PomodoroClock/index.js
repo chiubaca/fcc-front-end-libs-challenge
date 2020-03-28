e06bd14c943d6f97621528cad18812c76cfa9ceb
Alex Chiu <alexchiu11@gmail.com>
2020-03-28 17:36:17 +0000

    mover timer controls to new component and implemented increment and decrement funtions

--- src/pages/PomodoroClock/index.js
+++ src/pages/PomodoroClock/index.js
@@ -1,64 +1,109 @@
+/* eslint-disable jsx-a11y/accessible-emoji */
 import React from "react";
 import "./PomodoroClock.scss";
 
-export default function RandomQuoteMachine() {
+export default class PomodoroClock extends React.Component {
+  constructor(props) {
+    super(props);
+    this.incrementValHandler = this.incrementValHandler.bind(this);
+    this.decrementValHandler = this.decrementValHandler.bind(this);
+    this.state = {
+      sessionLen: 25,
+      breakLen: 5,
+    };
+  }
+
+  incrementValHandler(e) {
+    const timerToIncrement = e.target.attributes["data-type"].value;
+    console.log(timerToIncrement);
+    if (timerToIncrement === "break") {
+      this.setState({
+        breakLen: this.state.breakLen + 1,
+      });
+    } else if (timerToIncrement === "session") {
+      this.setState({
+        sessionLen: this.state.sessionLen + 1,
+      });
+    }
+  }
+
+  decrementValHandler(e) {
+    const timerToDecrement = e.target.attributes["data-type"].value;
+    console.log(timerToDecrement);
+    if (timerToDecrement === "break") {
+      this.setState({
+        breakLen: this.state.breakLen - 1,
+      });
+    } else if (timerToDecrement === "session") {
+      this.setState({
+        sessionLen: this.state.sessionLen - 1,
+      });
+    }
+  }
+
+  render() {
+    return (
+      <main id="pomodoro-clock">
+        <h1>Pomodoro Timer</h1>
+
+        <div id="controls-container">
+          <TimerControls
+            name="break"
+            decrementTime={this.decrementValHandler}
+            incrementTime={this.incrementValHandler}
+            time={this.state.breakLen}
+          />
+          <TimerControls
+            name="session"
+            decrementTime={this.decrementValHandler}
+            incrementTime={this.incrementValHandler}
+            time={this.state.sessionLen}
+          />
+        </div>
+
+        <div id="timer-label">
+          <p>Session</p>
+          <div id="time-left">25:00</div>
+        </div>
+
+        <div id="pomodoro-controls">
+          <button id="start_stop">
+            <span role="img" aria-label="start-pause">
+              ⏯️
+            </span>
+          </button>
+          <button id="reset">
+            <span role="img" aria-label="start-pause">
+              🔁
+            </span>
+          </button>
+        </div>
+      </main>
+    );
+  }
+}
+
+function TimerControls(props) {
   return (
-    <main id="pomodoro-clock">
-      <h1>Pomodoro Timer</h1>
-
-      <div id="controls-container">
-        <div class="length-controls">
-          <p id="break-label">Break Length</p>
-          <div>
-            <button id="break-decrement">
-              <span role="img" aria-label="decrement">
-                🔽
-              </span>
-            </button>
-            <div id="break-length">5</div>
-            <button id="break-increment">
-              <span role="img" aria-label="increment">
-                🔼
-              </span>
-            </button>
-          </div>
-        </div>
-
-        <div class="length-controls">
-          <p id="session-label"> Session Length </p>
-          <div>
-            <button id="session-decrement">
-              <span role="img" aria-label="decrement">
-                🔽
-              </span>
-            </button>
-            <div id="session-length">25</div>
-            <button id="session-increment">
-              <span role="img" aria-label="increment">
-                🔼
-              </span>
-            </button>
-          </div>
-        </div>
-      </div>
-
-      <div id="timer-label">
-        <p>Session</p>
-        <div id="time-left">25:00</div>
-      </div>
-
-      <div id="pomodoro-controls">
-        <button id="start_stop">
-          <span role="img" aria-label="start-pause">
-            ⏯️
-          </span>
+    <div className="length-controls">
+      <p id={props.name + "-label"}>{props.name} length</p>
+      <div>
+        <button
+          id={props.name + "-decrement"}
+          data-type={props.name}
+          onClick={props.decrementTime}
+        >
+          🔽
         </button>
-        <button id="reset">
-          <span role="img" aria-label="start-pause">
-            🔁
-          </span>
+        <div id={props.name + "-length"}>{props.time}</div>
+        <button
+          id={props.name + "-increment"}
+          data-type={props.name}
+          onClick={props.incrementTime}
+        >
+          🔼
         </button>
       </div>
-    </main>
+    </div>
   );
 }
